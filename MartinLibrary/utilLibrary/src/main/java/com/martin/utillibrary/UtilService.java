12b4bbec111a82fd2f78e8536a63aebfae4b7c6e
mrossi <martinrossi9009@gmail.com>
2021-02-09 17:57:32 -0300

    library

---
+++ MartinLibrary/utilLibrary/src/main/java/com/martin/utillibrary/UtilService.java
@@ -0,0 +1,11 @@
+package com.martin.utillibrary;
+
+import android.util.Log;
+
+class UtilService {
+
+    public void saludar(String name) {
+        System.out.println("111111111111111111");
+        Log.w("martinLog", "Holaaaa!: " + name);
+    }
+}
