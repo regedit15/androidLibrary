50720ba123de21400e3a3a9bfa461c30576ed747
mrossi <martinrossi9009@gmail.com>
2021-02-10 12:02:01 -0300

    libreria

--- MartinLibrary/utilLibrary/src/main/java/com/martin/utillibrary/UtilService.java
+++ MartinLibrary/utilLibrary/src/main/java/com/martin/utillibrary/UtilService.java
@@ -5,7 +5,7 @@ import android.util.Log;
 class UtilService {
 
     public void saludar(String name) {
-        System.out.println("111111111111111111");
+        System.out.println("1111111111111111112");
         Log.w("martinLog", "Holaaaa!: " + name);
     }
 }
