c9fa157031f98041faffc1689ec80ef08445c9a0
regedit15 <martinrossi9009@gmail.com>
2021-02-12 10:59:30 -0300

    a

--- MartinLibrary/utilLibrary/src/main/java/com/martin/utillibrary/Interfaces/LambdaInterface.java
+++
@@ -1,5 +0,0 @@
-package com.martin.utillibrary.Interfaces;
-
-public interface LambdaInterface {
-    public void execute();
-}
--- MartinLibrary/utilLibrary/src/main/java/com/martin/utillibrary/Interfaces/LambdaInterfaceWithOneParameterString.java
+++
@@ -1,5 +0,0 @@
-package com.martin.utillibrary.Interfaces;
-
-public interface LambdaInterfaceWithOneParameterString {
-    public void execute(String parametro);
-}
