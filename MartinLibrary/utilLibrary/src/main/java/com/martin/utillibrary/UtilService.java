bdc1d9fcfc5c0c47da2bfa03b256ba64e3bd0071
regedit15 <martinrossi9009@gmail.com>
2021-02-12 10:42:50 -0300

    libreria

---
+++ MartinLibrary/utilLibrary/src/main/java/com/martin/utillibrary/UtilService.java
@@ -0,0 +1,11 @@
+package com.martin.utillibrary;
+
+import android.util.Log;
+
+class UtilService {
+
+    public void saludar(String name) {
+        System.out.println("11111111111111111122");
+        Log.w("martinLog", "Holaaaa2!: " + name);
+    }
+}
